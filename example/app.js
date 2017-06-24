f9fdbea6e46e879315a79f75d89b4d4482b7b3fe
John Nolette <JNolette@Transparent.com>
2017-06-24 02:38:00 -0400

    Updated example with new features.

--- example/app.js
+++ example/app.js
@@ -1,29 +1,29 @@
 Vue.use(Localize);
-
+ 
 let localize = Localize.config({
-	default: 'en',
-	available: ['en', 'sp', 'pr', 'ch'],
-	not_found: '?',
-	localizations: {
-		en: {
-			header: {
-				title: 'English'
-			}
-		},
-		sp: {
-			header: {
-				title: 'Spanish'
-			}
-		},
-		pr: {
-			header: {
-				title: 'Portuguese'
-			}
-		}
-	}
+  default: 'en',
+  available: ['en-US', 'es-SP', 'pr-BR'],
+  fallback: '?',
+  localizations: {
+    "en-US": {
+      header: {
+        title: 'English'
+      }
+    },
+    "es-SP": {
+      header: {
+        title: 'Spanish'
+      }
+    },
+    "pr-BR": {
+      header: {
+        title: 'Portuguese'
+      }
+    }
+  }
 });
-
-new Vue({
-	el: '#app',
-	localize
-});
+ 
+ new Vue({
+   el: '#app',
+   localize
+ });
