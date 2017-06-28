66f24227df651926cca89ce7fb007f171f99803a
John Nolette <JNolette@Transparent.com>
2017-06-28 10:57:27 -0400

    Changes

--- src/locale.js
+++ src/locale.js
@@ -2,5 +2,6 @@ export default function (lang) {
   if (lang) {
     window.localStorage.setItem('localization', lang)  // # update session localization
     window.location.reload()  // # reload app with new localization
-  } else { return this.$options.localize.locale; }
+    // # left here, switch on mode -- hot will store all elems into virtual map to update -- otherwise reload
+  } else return this.$options.localize.locale
 }
