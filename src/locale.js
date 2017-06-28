cbaa201f6f62491f564074b9ef0afa9b2eb073a3
jnolette <jnolette@transparent.com>
2017-06-28 13:55:38 -0400

    Completed hot mode functionality, tested and functional.

--- src/locale.js
+++ src/locale.js
@@ -1,7 +1,15 @@
 export default function (lang) {
   if (lang) {
     window.localStorage.setItem('localization', lang)  // # update session localization
-    window.location.reload()  // # reload app with new localization
-    // # left here, switch on mode -- hot will store all elems into virtual map to update -- otherwise reload
+    switch (this.$options.localize.mode) {
+      case 'stale':
+        window.reload()  // # reload window with new locale
+        break
+      case 'hot':
+        Vue.directive('localize').update()  // # update all directive bindings
+        break
+      default:
+        if (this.$options.localize.debug) console.error('v-localize:\n  Mode could not be determined')
+    }
   } else return this.$options.localize.locale
 }
--- src/localize.js
+++ src/localize.js
@@ -1,34 +1,29 @@
 export default function (el, binding, vm) {
-  let localize = vm.context.$root.$options.localize
-  if (!binding.value) el.innerHTML = localize.locale
-  else {
-    try {
-      var regex = /([a-zA-Z$]{1,}).*?/g;
-      var localization = localize.localizations[binding.value.locale || localize.locale];
-      binding.value.item.match(regex).forEach(function (key) {
-        localization = localization[key]
-        if (localization === undefined && localize.debug) throw new Error('Cannot read property for ' + key + '.')
-      });
-      (!binding.value.attr) ? (el.innerHTML = localization) : (el.setAttribute(binding.value.attr, localization))
-      if (!binding.value.attr) {
-        el.innerHTML = localization;
-        ops = localize.available.find((loc) => {
-          return loc.locale == localize.locale;
-        });
-        if (ops) {
-          if (ops.orientation) el.setAttribute('dir', ops.orientation); // # change element display orientation
-          if (ops.font) {
-            if (ops.font.family) el.style.fontFamily = ops.font.family; // # https://www.w3schools.com/jsref/prop_style_fontfamily.asp
-            if (ops.font.size) el.style.fontSize = ops.font.size; // # https://www.w3schools.com/jsref/prop_style_fontsize.asp
-          };
+  const localize = vm.context.$root.$options.localize
+  try {
+    var localization = localize.localizations[binding.value.locale || localize.locale]
+    binding.value.item.match(localize.regex).forEach(function (key) {
+      localization = localization[key]
+      if (localization === undefined && localize.debug) throw new Error('Cannot read property for ' + key + '.')
+    })
+    if (!binding.value.attr) {
+      el.innerHTML = localization
+      const options = localize.available.find((loc) => {
+        return loc.locale == localize.locale  // # find options for locale if exists
+      })
+      if (options) {
+        if (options.orientation) el.setAttribute('dir', options.orientation)  // # change element display orientation
+        if (options.font) {
+          if (options.font.family) el.style.fontFamily = options.font.family  // # https://www.w3schools.com/jsref/prop_style_fontfamily.asp
+          if (options.font.size) el.style.fontSize = options.font.size  // # https://www.w3schools.com/jsref/prop_style_fontsize.asp
         };
-      }
-    } catch (e) {
-      if (localize.debug) {
-        console.error('v-localize:\n  Could not find localization for ' + binding.value.item + ' in ' + localize.locale + ' language.');
-        console.error(e)
       };
-      (!binding.value.attr) ? (el.innerHTML = localize.fallback) : (el.setAttribute(binding.value.attr, localize.fallback));
-    }
+    } else el.setAttribute(binding.value.attr, localization)
+  } catch (e) {
+    if (localize.debug) {
+      console.error('v-localize:\n  Could not find localization for ' + binding.value.item + ' in ' + localize.locale + ' language.');
+      console.error(e)
+    };
+    (!binding.value.attr) ? (el.innerHTML = localize.fallback) : (el.setAttribute(binding.value.attr, localize.fallback))
   }
 }
