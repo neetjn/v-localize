a45008e4fd3b0a708e56f2a5776a0daaabf91dcf
John Nolette <JNolette@Transparent.com>
2017-06-28 16:56:41 -0400

    Added structure to build config.

--- build/webpack.conf.js
+++ build/webpack.conf.js
@@ -1,6 +1,48 @@
 const path = require('path')
+const webpack = require('webpack')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 };
 
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: resolve('./dist'),
+    publicPath: 'dist/',
+    filename: 'v-localize.js',
+    library: ['VueI18next'],
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map';
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false,
+      },
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+    }),
+  ]);
+}
