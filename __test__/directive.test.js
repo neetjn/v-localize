d60d7acfc8914df079af92837b55eb6192cf74e5
John Nolette <jnolette@transparent.com>
2019-04-10 20:44:57 -0400

    Removing karma tests

--- __test__/directive.test.js
+++ __test__/directive.test.js
@@ -1,60 +1,21 @@
-import 'jest-dom/extend-expect'
-import fetch from 'node-fetch'
-import * as riot from 'riot'
-import PasswordKnown from '../dist/riot-is-password-known.js'
+require('jest-dom/extend-expect')
 
-// # pollyfill for fetch
-// eslint-disable-next-line
-window.fetch = fetch
+// const Vue = require('vue')
+// const Localize = require('../dist/v-localize')
 
-// # install password known plugin
-PasswordKnown(riot)
-
-riot.tag('root', `
-  <form>
-    <input type="password" data-is="password-known" />
-    <script>
-      const self = this
-
-      self.passwordStatus = null
-      self.on('passwordKnown', function(matched) {
-        self.passwordStatus = matched
-      })
-    </script>
-  </form>
-`)
+// const MockConfig = require('./mock.config')
 
 describe('Plugin', () => {
   beforeEach(() => {
     document.body.innerHTML = `
       <root />
     `
-    riot.mount('root')
   })
 
-  it('should install the tag as expected', function() {
-    const field = document.querySelector('input[type="password"]')
-    expect(field._tag).toBeDefined()
-  })
-
-  it('should bind tag field as expected', function(done) {
-    const context = this
-
-    const container = document.querySelector('root')
-    context.reached = false
-    container._tag.on('passwordKnown', function(result) {
-      expect(result.found).toBeTruthy()
-      expect(result.count).toBeGreaterThanOrEqual(23174662)
-      context.reached = true
-    })
-
-    const field = document.querySelector('input[type="password"]')
-    field.value = '123456'
-    field.dispatchEvent(new Event('keyup'))
-
-    setTimeout(function() {
-      expect(context.reached).toBeTruthy()
-      done()
-    }, 1500)
+  it('should install plugin as expected', function() {
+    const tag = document.querySelector('root')
+    console.log(tag)
+    // const field = document.querySelector('input[type="password"]')
+    // expect(field._tag).toBeDefined()
   })
 })
---
+++ __test__/mock.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  debug: true,
+  default: 'en-US',
+  available: ['en-US', 'es-SP', 'pr-BR', {
+    locale: 'ar-MS',
+    orientation: 'rtl',
+    font: {
+      size: 'smaller'
+    }
+  }],
+  fallback: '?',
+  localizations: {
+    'en-US': {
+      header: {
+        title: 'English'
+      }
+    },
+    'es-SP': {
+      header: {
+        title: 'Spanish'
+      }
+    },
+    'pr-BR': {
+      header: {
+        title: 'Portuguese'
+      }
+    },
+    'ar-MS': {
+      header: {
+        title: 'Arabic'
+      }
+    }
+  }
+}
