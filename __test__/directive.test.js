e4bd88be17edf08cb06461e44cdd3e535dc6bf60
John Nolette <jnolette@transparent.com>
2019-04-11 07:13:32 -0400

    Adding vue test utils

--- __test__/directive.test.js
+++ __test__/directive.test.js
@@ -1,18 +1,15 @@
 require('jest-dom/extend-expect')
-
-const Vue = require('vue')
+const { mount, createLocalVue } = require('vue-test-utils')
 const Localize = require('../dist/v-localize')
-
 const MockConfig = require('./mock.config')
 
 describe('Plugin', () => {
+  const context = {}
+
   beforeEach(() => {
     document.body.innerHTML = `
       <root />
     `
-
-    Vue.use(Localize)
-    const localize = Localize.config(MockConfig)
     const RootComponent = {
       template: `
       <div id="root">
@@ -20,18 +17,22 @@ describe('Plugin', () => {
       </div>
       `
     }
-    const App = Vue.extend(RootComponent)
-    // eslint-disable-next-line
-    const vm = new App({
-      localize
-    }).$mount('root')
+    const Vue = createLocalVue()
+    Vue.use(Localize)
+    const localize = Localize.config(MockConfig)
+    const App = mount(RootComponent, { Vue, localize })
+
+    context.App = App
   })
 
   it('should install plugin as expected', function() {
-    const tag = document.querySelector('root')
-    console.log(tag)
-    console.log(document.body.innerHTML)
+    // const tag = document.querySelector('root')
+    // console.log(tag)
+    // console.log(document.body.innerHTML)
     // const field = document.querySelector('input[type="password"]')
     // expect(field._tag).toBeDefined()
+    // console.log(context.App.find('h1'))
+    // left here, figure out why mixin method is not being propagated
+    console.log(context.App.vm)
   })
 })
